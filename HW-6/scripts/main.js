91748bb7efcf39b5c6c0db145c5479c655aa3ae0
Jacqueline Evans-Shaw <39539456+Jackie98@users.noreply.github.com>
2019-02-24 17:26:22 -0700

    HW-6 Edits

--- HW-6/scripts/main.js
+++ HW-6/scripts/main.js
@@ -1,4 +1,7 @@
-var imageNames = ["image1", "image2", "image3", "image4", "image 5", "image 6", "image 7", "image 8", "image 9", "image 10", "image 11", "image 12"];
+// since you only had 6 images on your HW6Game, you only need 6 imageNames here.
+// also, you have to make sure there are no spaces in the names (look at image 5, image 6)
+//var imageNames = ["image1", "image2", "image3", "image4", "image 5", "image 6", "image 7", "image 8", "image 9", "image 10", "image 11", "image 12"];
+var imageNames = ["image1", "image2", "image3", "image4", "image5", "image6"];
 var blankImagePath = "imgs/gofish.png";
 var firstNumber = -1;
 var secondNumber = -1;
@@ -26,11 +29,21 @@ function printBlanks()
 function createRandomImageArray()
 {
     // create an array of actual images
-    var actualImagePath = ["imgs/sun.jpg", "imgs/moon.jpg", "imgs/venus.jpg", "imgs/mars.jpg"];
+    // since you have only 6 images on the main page, you need only 3 in this array
+    // if you want to add more, you just need to create more images on the HW6Game page.
+    // also, the sun.jpg wasn't working quite right for some reason.
+    //var actualImagePath = ["imgs/sun.jpg", "imgs/moon.jpg", "imgs/venus.jpg", "imgs/mars.jpg"];
+    var actualImagePath = ["imgs/mars.jpg", "imgs/moon.jpg", "imgs/venus.jpg"];
     // create another array to make sure the images only get added twice
-    var count = [0,0,0,0,0,0,0,0,0,0,0,0];
+
+    // in this case, you only need 3 in this array because you are checking
+    // to see if each image (from the actualImagePath) is added twice
+    //var count = [0,0,0,0,0,0,0,0,0,0,0,0];
+    var count = [0,0,0];
     // create a while statement to check to see if our actual image array is full
-    while(actualImages.length < 12)
+    // since you have only 3 images, you only have to check up to 10
+    //while(actualImages.length < 12)
+    while(actualImages.length < 6)
     {
         // get a random number between 0 and the number total number of images that we can choose from
         var randomNumber = Math.floor(Math.random() * actualImagePath.length)
@@ -43,10 +56,6 @@ function createRandomImageArray()
             count[randomNumber] = count[randomNumber] + 1;
         }
     }
-
-
-
-
 
 }
 
